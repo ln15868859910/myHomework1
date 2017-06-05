058d5055e04541a3e372dfc6ae36785981c1d0f1
luhao <hao.lu@xiaobaoonline.com>
2017-06-05 17:41:31 +0800

    model对话框组件

--- examples/routers/modal.vue
+++ examples/routers/modal.vue
@@ -1,34 +1,72 @@
 <template>
     <div>
-        <Button type="primary" @click="modal1 = true">显示对话框</Button>
-        
-        <Modal
-                v-model="modal1"
-                :title="title"
-                @on-ok="ok"
-                :mask-closable="false"
-                @on-cancel="cancel">
-            <p><Button type="ghost" @click="title = '这是标题'">设置标题</Button> {{title}}</p>
-            <p>对话框内容</p>
-            <p>对话框内容</p>
+        <Button type="primary" @click="modal1 = true">基本状态</Button>
+        <Button type="primary" @click="modal2 = true">表单对话框</Button>
+    
+        <Modal v-model="modal1" :closable="false" class-name="vertical-center-modal" width="380">
+            <p slot="header" style="text-align:center">
+                <span style="color: #fff;">提示</span>
+            </p>
+            <div style="text-align:center;height:30px;font-size: 14px;line-height: 30px;">
+                解除绑定？
+            </div>
+            <div slot="footer">
+                <Button type="ghost" @click="cancel">取消</Button>
+                <Button type="primary" @click="ok">确定</Button>
+            </div>
+        </Modal>
+    
+        <Modal v-model="modal2" :closable="false" class-name="vertical-center-modal" width="380">
+            <p slot="header" style="text-align:center">
+                <span style="color: #fff;">修改手机号</span>
+            </p>
+            <div>
+                <div class="form-item">
+                    <label class="form-label">密码</label>
+                    <Input class="form-input" v-model="value" placeholder=""></Input>
+                </div>
+                <div class="form-item">
+                    <label class="form-label">新手机号</label>
+                    <Input class="form-input" v-model="value" placeholder=""></Input>
+                </div>
+                <div class="form-item">
+                    <label class="form-label">验证码</label>
+                    <Input class="form-input" style="width:82px;margin-right:6px;" v-model="value" placeholder=""></Input>
+                    <Button type="primary" @click="countDown">{{ code }}</Button>
+                </div>
+            </div>
+            <div slot="footer">
+                <Button type="ghost" @click="cancel2">取消</Button>
+                <Button type="primary" @click="ok2">确定</Button>
+            </div>
         </Modal>
     </div>
 </template>
 <script>
-    export default {
-        data () {
-            return {
-                modal1: true,
-                title:''
-            }
+export default {
+    data() {
+        return {
+            modal1: false,
+            modal2: false,
+            value: "",
+            code: "获取验证码"
+        }
+    },
+    methods: {
+        ok() {
+            this.modal1 = false;
         },
-        methods: {
-            ok () {
-                this.$Message.info('点击了确定');
-            },
-            cancel () {
-                this.$Message.info('点击了取消');
-            }
+        cancel() {
+            this.modal1 = false;
+        },
+        ok2() {
+            this.modal2 = false;
+        },
+        cancel2() {
+            this.modal2 = false;
+        },
+        countDown() {
         }
     }
+}
 </script>
