bbea02060231b071d4d695cfa1222142fec01b43
li.lv <li.lv@xiaobaoonline.com>
2017-07-18 14:28:38 +0800

    筛选联动组件：根据model渲染对应基础组件

--- src/components/business/consultFilter/filter-union.vue
+++ src/components/business/consultFilter/filter-union.vue
@@ -1,25 +1,65 @@
 <template>
-    <div>{{value}}</div>
+    <div>
+        <component v-bind:model="model"  v-bind:is="currentView">
+    
+        </component>
+    </div>
 </template>
 <script>
+import { Select, Option, OptionGroup } from '../../select';
 
-    export default {
-        name: 'consultFilterUnion',
-        props: {},
-        data () {
-            return {
-                value:"筛选子组件-联动"
-            };
-        },
-        computed: {
-          
-        },
-        mounted () {
-          
-        },
-        methods: {
-          
+const ConsultFilterUnionComponent = {
+    //template: '<h3 v-for="item in items">{{ item.c}}</h3>',
+    props: {
+        model: {
+            require: true
         }
-    };
+    },
+    data() {
+        return {
+            //items : myData
+        }
+    },
+    render: function (createElement) {
+        if (this.model.type == 1) {
+            return createElement(Select)
+        }
+        if (this.model.type == 2) {
+            return createElement(Select)
+        }
+
+    }
+
+};
+export default {
+    name: 'consultFilterUnion',
+    props: {
+        myModel:{
+            require:true
+        }
+    },
+    components: {
+        ConsultFilterUnionComponent
+    },
+    data() {
+        return {
+            model: {
+                type: 1,
+                key: "single",
+                text: "单选"
+            },
+            currentView: ConsultFilterUnionComponent
+        };
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
 
 </script>
