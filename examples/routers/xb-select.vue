abc871edbcd177f498e663e1954d7351c4cdbb60
陈雪冬 <xuedong.chen@xiaobaoonline.com>
2017-10-23 19:35:43 +0800

    更新xb-select

--- examples/routers/xb-select.vue
+++ examples/routers/xb-select.vue
@@ -4,10 +4,23 @@
     <div style="margin-left:150px">
         <h3>xb-Select</h3>
         <br/><br/>
+        <div>{{JSON.stringify(selectdata,null,4)}}</div>
          本地搜索
-        <Xb-Select :list-data="listdata" :selected-data="selectdata" @confirm="selectdone" remote></Xb-Select>
+        <Xb-Select :list-data="listdata" :selected-data="selectdata" @confirm="selectdone" remote filterable></Xb-Select>
+
         正常下拉 无搜索
         <Xb-Select :list-data="listdata" :selected-data="selectdata" @confirm="selectdone"></Xb-Select>
+
+        <h2>自定义</h2>
+        <Xb-Select :list-data="listdata" :selected-data="selectdata" @confirm="selectdone" valueKey="value">
+            <template slot="lislot" scope="item">
+                    <span style="display:inline-block;background:#aaa;">{{item.data.name}}</span>
+                    <span style="display:inline-block;background:#bbb;">{{item.data.value}}</span>
+            </template>
+        </Xb-Select>
+        
+        <Xb-Select :list-data="listdata"  valueKey="value" v-model="value.a" :showdelete="false"></Xb-Select>
+
     </div>
 </template>
 <script>
@@ -15,7 +28,7 @@ export default {
     data() {
         return {
             listdata: [{
-                id: 'shanghai',
+                value: 'shanghai',
                 name: '上海市'
             },
             {
@@ -37,6 +50,9 @@ export default {
             selectdata: {
                 value: 'chengdu',
                 name: '成都'
+            },
+            value:{
+
             }
         };
     },
@@ -45,7 +61,7 @@ export default {
     },
     methods: {
         selectdone(data) {
-
+            this.value.a = data.value;
             console.log(data);
             this.selectdata =  data;
         },
--- src/components/xb-select/select.vue
+++ src/components/xb-select/select.vue
@@ -1,26 +1,30 @@
 <template>
     <div :class="classes" v-clickoutside="hideMenu" style="position: relative;">
         <div class="ivu-select-selection" @click="toggleMenu" :class="[prefixCls2 + '-selection']">
-            <span class="ivu-select-placeholder" v-if="!selectedData.name">请选择</span>
-            <input class="ivu-select-input" type="text" v-model="selectedData.name" readonly style="width:100%;" @focus="showClose=true">
-            <i class="ivu-icon ivu-icon-close" :class="[prefixCls2 + '-close']" v-show="showdelete&&selectedData.name" @click.stop.prevent="deleteSelect($event)"></i>
-            <Icon type="arrow-down" class="ivu-select-arrow" style="position: absolute;right: 10px;top: 13px;display: inline-block;"></Icon>
+            <span class="ivu-select-placeholder" v-if="!query">请选择</span>
+            <input class="ivu-select-input" type="text" v-model="query" style="width:100%;" v-show="filterable" @input="onchange" @on-enter="search" @blur="handleBlur">
+            <input class="ivu-select-input" type="text" v-model="query" readonly style="width:100%;" v-show="!filterable">
+            <Icon type="close" :class="[prefixCls + '-arrow']" v-show="showdelete&&query" @click.native.stop="deleteSelect"></Icon>
+            <Icon type="arrow-down" :class="[prefixCls + '-arrow']" v-show="!remote"></Icon>
         </div>
 
         <transition :name="transitionName">
             <div class="ivu-select-dropdown" x-placement="bottom" v-if="visible" style="width:100%;position: absolute;padding:10px;">
-                <i-input v-model="query" placeholder="请输入..." icon="search" :maxlength=100 @blur="handleBlur" @keydown="resetInputState" @keydown.delete="handleInputDelete" @on-enter="search" @on-click="search" class="new-search" style="width:100%;" v-if="remote" @on-change="onchange"></i-input>
-                <ul class="ivu-select-not-found" v-if="listData.length==0">
+                <ul class="ivu-select-not-found" v-if="listData.length==0||(searching&&!this.searchlistData.length)">
                     <li>无匹配数据</li>
                 </ul>
                 <ul class="ivu-select-dropdown-list" v-show="!searching">
-                    <li class="ivu-select-item" v-for="item in listData" :key="item.id" @click="selectThis(item)">{{item.name}}</li>
-                    <li class="ivu-select-item ivu-select-item-disabled" v-if="remote">【更多选项请搜索】</li>
+                    <li class="ivu-select-item" v-for="item in listData" :key="item[valueKey]" @click="selectThis(item)">
+                        <slot :data="item" name="lislot">{{item[labelKey]}}</slot>
+                    </li>
+                    <li class="ivu-select-item ivu-select-item-disabled" v-if="remote&&remoteMethod">【更多选项请搜索】</li>
                 </ul>
                 <ul class="ivu-select-dropdown-list" v-show="searching">
-                    <li class="ivu-select-item" v-for="item in searchlistData" :key="item.id" @click="selectThis(item)">{{item.name}}</li>
+                    <li class="ivu-select-item" v-for="item in searchlistData" :key="item[valueKey]" @click="selectThis(item)">
+                        <slot :data="item" name="lislot">{{item[labelKey]}}</slot>
+                    </li>
                 </ul>
-                <ul class="ivu-select-loading" style="display: none;">加载中</ul>
+                <ul class="ivu-select-loading" v-show="loading">加载中</ul>
             </div>
         </transition>
     </div>
@@ -39,6 +43,9 @@ export default {
     components: { Icon },
     directives: { clickoutside },
     props: {
+        value:{
+            type:[String,Number]
+        },
         listData: {
             //下拉列表
             type: Array
@@ -47,7 +54,14 @@ export default {
             //选择的数据、默认数据
             type: Object
         },
+        filterable:{
+            type:Boolean,
+            default:false
+        },
         remoteMethod: {
+            type: Function
+        },
+        filterMethod:{
             type: Function
         },
         remote: {
@@ -57,6 +71,18 @@ export default {
         showdelete:{
             type: Boolean,
             default: true
+        },
+        valueKey:{
+            type:String,
+            default:'id'
+        },
+        labelKey:{
+            type:String,
+            default:'name'
+        },
+        loading:{
+            type:Boolean,
+            default:false
         }
     },
     data() {
@@ -69,6 +95,8 @@ export default {
             showClose: false,
             searching: false,//过滤状态
             lastData: {},
+            nullble:false,
+            model:'',
             searchlistData:[]
         };
     },
@@ -88,20 +116,39 @@ export default {
         },
         transitionName: function() {
             return 'slide-up';
+        },
+        showCloseIcon () {
+            return !this.multiple && this.showdelete;
+        },
+        currentLable(){
+            if(this.value){
+                return this.findValueLabel();
+            }else{
+                return this.selectedData[this.labelKey];
+            }
         }
     },
     created: function() {
         this.lastData = Object.assign({}, this.selectedData);
     },
     methods: {
-        handleBlur() { },
+        findValueLabel(){
+            var obj = this.listData.filter(item=>{
+                var id = this.model;
+                return item[this.valueKey] === id;
+            });
+            return obj.length ? obj[0][this.labelKey] : '';
+        },
+        handleBlur() { 
+            this.visible = false;
+            this.searching = false;
+            this.updateQuery();
+        },
         resetInputState() {
             this.inputLength = this.$refs.input.value.length * 12 + 20;
         },
         handleInputDelete() {
-            if (this.multiple && this.model.length && this.query === '') {
-                this.removeTag(this.model.length - 1);
-            }
+            
         },
         toggleMenu() {
             if (this.disabled) {
@@ -118,9 +165,15 @@ export default {
             this.visible = false;
             this.showClose = false;
             this.lastData = Object.assign({}, data);
+            this.$emit('input', data[this.valueKey]);
+            this.model = data[this.valueKey];
+            if(this.nullble){
+                this.updateQuery();
+            }
             this.$emit('confirm', data);
         },
         onchange(){
+            this.searching = true;
             //只有本地搜索时才会实时？？
             if(this.remoteMethod){
                 return;
@@ -131,30 +184,51 @@ export default {
             this.search();
         },
         search() {
-            if (this.remoteMethod) {
+            if (this.remoteMethod && typeof this.remoteMethod == 'function') {
                 this.remoteMethod(this.query);
             } else if (this.remote) {
-                this.searching = true;
-                this.searchlistData = this.listData.filter((text) => {
-                    return text.name.indexOf(this.query) > -1;
-                });
+                if(this.filterMethod && typeof this.filterMethod == 'function'){
+                    this.searchlistData = this.listData.filter(this.filterFnc);
+                }else{
+                    this.searchlistData = this.listData.filter((text) => {
+                        return text[this.labelKey].indexOf(this.query) > -1;
+                    });
+                }
+
             }
         },
         deleteSelect() {
             var obj = Object.assign({}, this.lastData);
-            obj.name = '';
-            obj.id = 0;
-            obj.Id = 0;
+            obj[this.labelKey] = '';
+            obj[this.valueKey]= 0;
+
+            this.model = 0;
+            this.updateQuery();
             this.$emit('confirm', obj);
+        },
+        updateQuery(){
+            if((this.value===''||this.value===undefined)&&!this.selectedData){
+                this.nullble = true;
+                this.query = this.findValueLabel();
+                return;
+            }
+            if(this.value===0){
+                this.query = '';
+                this.model = 0;
+            }
+            this.query =  (this.value!==''&&this.value!==undefined)? this.findValueLabel() : this.selectedData[this.labelKey];
         }
     },
-
+    mounted(){
+        this.updateQuery();
+    },
     watch: {
-        query() {
-            if (this.remoteMethod) {
-                this.remoteMethod(this.query);
-            }else if(this.remote){
-                this.searching = !!this.query;
+        selectedData(){
+            this.updateQuery();
+        },
+        value(val){
+            if(val!==''||val!==undefined){
+                this.updateQuery();
             }
         }
     }
