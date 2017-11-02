7f211780dd0088d9a5c3b701570272461f98c600
陈雪冬 <xuedong.chen@xiaobaoonline.com>
2017-11-02 10:46:38 +0800

    数字输入键组件优化

--- src/components/xb-input-number/input-number.vue
+++ src/components/xb-input-number/input-number.vue
@@ -2,15 +2,15 @@
     <Xb-Poptip mistake title="提示标题" :content="errcontent" placement="top" :show="iferror">
         <div :class="wrapClasses" style="width:100%;">
             <div :class="handlerClasses" v-if="!hidestep">
-                <a @click="up" @mouse.down="preventDefault" :class="upClasses">
+                <a @click="up" @mousedown="preventDefault" :class="upClasses">
                     <span :class="innerUpClasses" @click="preventDefault"></span>
                 </a>
-                <a @click="down" @mouse.down="preventDefault" :class="downClasses">
+                <a @click="down" @mousedown="preventDefault" :class="downClasses">
                     <span :class="innerDownClasses" @click="preventDefault"></span>
                 </a>
             </div>
             <div :class="inputWrapClasses">
-                <input :class="inputClasses" :disabled="disabled" :placeholder="placeholder" autocomplete="off" :autofocus="autofocus" @focus="focus" @blur="blur" @keydown.stop="keyDown" @change="change" :name="name" :value="currentValue" :style="this.hidestep?'text-align:right':'text-align:left'">
+                <input :class="inputClasses" :disabled="disabled" :placeholder="placeholder" autocomplete="off" :autofocus="autofocus" @focus="focus" @blur="blur" @keydown.stop="keyDown" @input="change" @change="change" :name="name" :value="precisionValue" :style="this.hidestep?'text-align:right':'text-align:left'">
             </div>
         </div>
     </Xb-Poptip>
@@ -22,9 +22,6 @@ import Emitter from '../../mixins/emitter';
 const prefixCls = 'ivu-input-number';
 const iconPrefixCls = 'ivu-icon';
 
-function isValueNumber(value) {
-    return (/(^-?[0-9]+\.{1}\d+$)|(^-?[1-9][0-9]*$)|(^-?0{1}$)/).test(value + '');
-}
 function addNum(num1, num2) {
     let sq1, sq2, m;
     try {
@@ -46,7 +43,7 @@ function addNum(num1, num2) {
     //            return (num1 * m + num2 * m) / m;
     //        }
     m = Math.pow(10, Math.max(sq1, sq2));
-    return (num1 * m + num2 * m) / m;
+    return (Math.round(num1 * m) + Math.round(num2 * m)) / m;
 }
 
 export default {
@@ -66,7 +63,7 @@ export default {
             default: 1
         },
         value: {
-            type: Number | String
+            type: Number|String
         },
         size: {
             validator(value) {
@@ -108,13 +105,17 @@ export default {
             upDisabled: false,
             downDisabled: false,
             iferror: false,
-            errcontent: ''
-            // currentValue: this.value
+            errcontent: '',
+            currentValue: this.value
         };
     },
     computed: {
-        currentValue() {
-            return this.value;
+        precisionValue () {
+            // can not display 1.0
+            if(this.currentValue===''){
+                return '';
+            }
+            return this.fixed ? this.fiexdNumber(this.currentValue) : this.currentValue;
         },
         wrapClasses() {
             return [
@@ -215,6 +216,8 @@ export default {
             this.setValue(val);
         },
         setValue(val) {
+            if (!isNaN(this.fixed)) val = Number(Number(val).toFixed(this.fixed));
+
             this.$nextTick(() => {
                 this.currentValue = val;
                 this.$emit('input', val);
@@ -227,7 +230,7 @@ export default {
         },
         blur() {
             this.focused = false;
-            this.$emit('on-blur');
+            // this.$emit('on-blur');
         },
         keyDown(e) {
             if (e.keyCode === 38) {
@@ -282,15 +285,15 @@ export default {
         },
         change(event) {
             let val = event.target.value.trim();
+            if (event.type == 'input' && val.match(/^\-?\.?$|\.$/)) return; // prevent fire early if decimal. If no more input the change event will fire later
+            if (event.type == 'change' && Number(val) === this.currentValue) return; // already fired change for input event
 
             const max = this.max;
             const min = this.min;
 
-            if (isValueNumber(val)) {
-                val = Number(val);
-                if (this.fixed > -1) {
-                    val = this.fiexdNumber(val);
-                }
+            const isEmptyString = val.length === 0;
+            val = Number(val);
+            if (!isNaN(val) && !isEmptyString) {
                 this.currentValue = val;
 
                 if (val > max) {
@@ -304,7 +307,7 @@ export default {
                     this.setValue(val);
                 }
             } else {
-                if(val!=""){
+                if(isNaN(val)){
                     this.setError('nan');
                 }else{
                     if(this.required){
@@ -331,8 +334,8 @@ export default {
             if (this.hidestep) {
                 return;
             }
-            if (isValueNumber(val) || val === 0) {
-                val = Number(val);
+            val = Number(val);
+            if (!isNaN(val)) {
                 const step = this.step;
 
                 this.upDisabled = val + step > this.max;
@@ -347,8 +350,17 @@ export default {
         this.changeVal(this.currentValue);
     },
     watch: {
+        value (val) {
+            this.currentValue = val;
+        },
         currentValue(val) {
             this.changeVal(val);
+        },
+        min () {
+            this.changeVal(this.currentValue);
+        },
+        max () {
+            this.changeVal(this.currentValue);
         }
     }
 };
