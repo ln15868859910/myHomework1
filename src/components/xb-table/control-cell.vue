4e31e8e0e4e369f852fd17e66519f1daab0a9741
陈雪冬 <xuedong.chen@xiaobaoonline.com>
2018-01-09 17:16:46 +0800

    警告修复

--- src/components/xb-table/control-cell.vue
+++ src/components/xb-table/control-cell.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <template v-if="controldata.length<3">
-            <i-button :class="[prefixCls+'-control-btn']"  type="text" v-for="(btn,bindex) in controldata" :disabled="btn.disabled" @click="handlecontrolclick(btn)">
+            <i-button :class="[prefixCls+'-control-btn']"  type="text" v-for="(btn,bindex) in controldata" :key="bindex" :disabled="btn.disabled" @click="handlecontrolclick(btn)">
                 <span v-if="!btn.render">{{btn.title}}</span>
                 <Cell
                      v-else
