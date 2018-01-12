af9a910f9944e1413f3cf39b7c8a4b2e1a853c0b
ccy_fish <yu.chen@xiaobao100.com>
2018-01-12 17:42:26 +0800

    201801_erp_pinyin_spec

--- src/components/xb-fuzzy-select/fuzzyselect.vue
+++ src/components/xb-fuzzy-select/fuzzyselect.vue
@@ -83,7 +83,7 @@ export default {
             return this.searchArea.selected.text == '学员姓名'
         },
         vplaceholder(){
-            var t = "请输入" + searchArea.selected.text;
+            var t = "请输入" + this.searchArea.selected.text;
             if(this.searchArea.selected.text == '学员姓名'){
                 t  + "，支持拼音缩写"
             }
