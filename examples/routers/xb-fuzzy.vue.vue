9a546d81c72842f7c332f3e840c278a5d9bceb0b
陈雪冬 <xuedong.chen@xiaobaoonline.com>
2018-03-14 17:43:00 +0800

    修复 fuzzy的示例缺失

---
+++ examples/routers/xb-fuzzy.vue.vue
@@ -0,0 +1,22 @@
+<template>
+   
+   <h1>{{msg}}</h1>
+</template>
+
+<script>
+
+export default {
+   
+    data(){
+        return{
+            msg:'xb-fuzzy'
+        };
+    }
+};
+</script>
+
+<style>
+.warning{
+    color:red;
+}
+</style>
